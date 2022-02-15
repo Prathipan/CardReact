8189922fe64fa61bca79f9a327ca637febb0d85a
Prathipan <prathipan24p@gmail.com>
2022-02-15 17:24:14 +0530

    First commit

---
+++ src/subscribeButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Subscribe(){
+    return(
+        <div className="d-grid">
+            <button class="btn btn-primary text-uppercase">subscribe</button>
+          </div>
+    )
+}
+
+export default Subscribe;
